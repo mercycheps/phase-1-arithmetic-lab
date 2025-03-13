bbe41883a78bc6198d83fd59de9e50535d8f36cf
Mercy <maritimmercy8@gmail.com>
2025-03-13 10:55:26 +0300

    added function

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
 
-// Write your code here
+let num1 = 2;
+let num2 = 31;
+const multiply = num1 * num2
+
+let random = Math.floor(Math.random() * 10) + 1;
+
+let num3 = 10;
+let num4 = 6;
+const mod = num3 % num4
+
+
+let max = Math.max(1, 20, 3, 14, 5);
+
+
+
